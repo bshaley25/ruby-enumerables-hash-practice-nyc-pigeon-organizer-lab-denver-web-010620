5e90d322f5a81a8ca72de2e07e05c672255fc8d4
Bradley Haley <bshaley25@gmail.com>
2019-12-17 19:48:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,10 +15,13 @@ def nyc_pigeon_organizer(data)
              new_attribute = key_1.to_s
             
              if result[name][key] == nil
+                new_attribute = key_1.to_s
                 result[name][key] = [new_attribute]
              else
                 result[name][key] << new_attribute
              end
+
+            end
 
         end
     end
